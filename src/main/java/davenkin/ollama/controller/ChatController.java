07662ee460c98544a0bbe41ad5053dd1980d0d4c
Davenkin <davenkin@126.com>
2024-07-23 13:02:29 +0800

    can upload text message

--- src/main/java/davenkin/ollama/controller/ChatController.java
+++ src/main/java/davenkin/ollama/controller/ChatController.java
@@ -1,8 +1,11 @@
 package davenkin.ollama.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import davenkin.ollama.service.ChatService;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
   private final ChatService chatService;
+  private final VectorStore vectorStore;
 
-  public ChatController(ChatService chatService) {this.chatService = chatService;}
+  public ChatController(ChatService chatService, VectorStore vectorStore) {
+    this.chatService = chatService;
+    this.vectorStore = vectorStore;
+  }
 
   @GetMapping
   public Map<String, String> chat(@RequestParam(name = "question") String question) {
-    return Map.of("result", this.chatService.answerFor(question));
+    return Map.of("answer", this.chatService.answerFor(question));
+  }
+
+  @GetMapping(value = "/upload")
+  public Map<String, String> upload(@RequestParam(name = "data") String data) {
+    this.vectorStore.add(List.of(new Document(data)));
+    return Map.of("result", "Upload success!");
   }
 }
